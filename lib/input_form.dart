91c2301d13ccdf72be6360b41a9b218eee9fd679
yusaku-hayashi <22520603+yusaku-hayashi@users.noreply.github.com>
2022-07-09 15:01:19 +0900

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lib/input_form.dart

---
+++ lib/input_form.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class InputForm extends StatelessWidget {
+  const InputForm({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Expanded(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            DropdownButton<String>(
+              isExpanded: true,
+              icon: const Icon(Icons.arrow_downward),
+              onChanged: (String? newValue) {},
+              items: <String>[
+                'Keep',
+                'Problem',
+                'Try',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            TextFormField(
+              minLines: 10,
+              maxLines: 10,
+              decoration: InputDecoration(
+                labelText: 'Description',
+                border: OutlineInputBorder(),
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
